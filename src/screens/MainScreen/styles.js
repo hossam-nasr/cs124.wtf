4056db340275d1dc86c80af08bfd9f51d81e6fca
Hossam <hossam.nasr99@gmail.com>
2019-08-05 22:30:06 +0200

    responsivenessgit add .

--- src/screens/MainScreen/styles.js
+++ src/screens/MainScreen/styles.js
@@ -5,11 +5,14 @@ export const Container = styled.div`
   min-width: 100vw;
   min-height: 100vh;
   background-image: url(${JelaniDeepFried});
-  background-size: cover;
+  background-size: contain;
+  background-position: center;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
+  background-repeat: space;
+  background-color: black;
 `;
 
 export const Title = styled.div`
@@ -17,4 +20,12 @@ export const Title = styled.div`
   font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New";
   text-shadow: 0px 0px 30px rgba(0, 0, 0, 1);
   color: white;
+
+  @media all and (max-width: 800px) {
+    font-size: 60px;
+  }
+
+  @media all and (max-width: 500px) {
+    font-size: 40px;
+  }
 `;
