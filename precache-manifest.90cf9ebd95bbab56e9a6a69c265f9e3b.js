9a1e8c79bb66f69e486e1be9650c476c1c94ba5e
Hossam <hossam.nasr99@gmail.com>
2019-08-05 21:29:37 +0200

    Updates

---
+++ precache-manifest.90cf9ebd95bbab56e9a6a69c265f9e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7c6a10993c1ce9bf48782c72fbb0e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf6d4695b25249c2573",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "87476944b82569caf4b1",
+    "url": "/static/js/2.3f0bc0d2.chunk.js"
+  },
+  {
+    "revision": "ccf6d4695b25249c2573",
+    "url": "/static/js/main.0de8252f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ab720047761a722758b7646d63e18e7",
+    "url": "/static/media/jelani-deep-fried.6ab72004.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.90cf9ebd95bbab56e9a6a69c265f9e3b.js"
 );
 
 self.addEventListener('message', (event) => {
